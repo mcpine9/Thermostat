25a4f4fc4e9c8b9be66b07a03054761a6a9b093b
Mark Pine <mark.pine@icainformatics.com>
2014-09-12 13:12:48 -0500

    Added DHT11 & 22 sensor driver code.

--- Thermostat/Program.cs
+++ Thermostat/Program.cs
@@ -1,27 +1,54 @@
-﻿using System.Threading;
-using Microsoft.SPOT.Hardware;
-using SecretLabs.NETMF.Hardware.NetduinoPlus;
-
-namespace Thermostat
+﻿//---------------------------------------------------------------------------
+//<copyright file="Program.cs">
+//
+// Copyright 2011 Stanislav "CW" Simicek
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//</copyright>
+//---------------------------------------------------------------------------
+namespace DhtSensorApp
 {
-    public class Program
+  using System.Threading;
+  using CW.NETMF;
+  using CW.NETMF.Sensors;
+  using Microsoft.SPOT;
+  using SecretLabs.NETMF.Hardware.Netduino;
+
+  /// <summary>
+  /// This application demonstrates the usage of DHT11/DHT22 sensors.
+  /// </summary>
+  public static class Program
+  {
+    public static void Main()
     {
-        public static void Main()
+      // TODO: Change pins and pull-up resistor type according to the actual wiring
+      var dhtSensor = new Dht11Sensor(Pins.GPIO_PIN_D0, Pins.GPIO_PIN_D1, PullUpResistor.External);
+    //var dhtSensor = new Dht22Sensor(Pins.GPIO_PIN_D0, Pins.GPIO_PIN_D1, PullUpResistor.External);
+
+      while(true)
+      {
+        Thread.Sleep(2000);
+
+        if(dhtSensor.Read())
         {
-            // write your code here
-            var ledPort = new OutputPort(Pins.ONBOARD_LED, false);
-
-            while (true)
-            {
-                ledPort.Write(true);
-                Thread.Sleep(1000);
-
-                ledPort.Write(false);
-                Thread.Sleep(1000);
-            }
-
-
+          Debug.Print("DHT sensor Read() ok, RH = " + dhtSensor.Humidity.ToString("F1") + "%, Temp = " + dhtSensor.Temperature.ToString("F1") + "°F");
         }
-
+        else
+        {
+          Debug.Print("DHT sensor Read() failed");
+        }
+      }
     }
+  }
 }
