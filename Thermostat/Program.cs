63d64b1c47085cae012c7ef7d44c910b8b70902f
Mark Pine <mark.pine@icainformatics.com>
2014-09-12 13:02:39 -0500

    Initial commit

---
+++ Thermostat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace Thermostat
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            var ledPort = new OutputPort(Pins.ONBOARD_LED, false);
+
+            while (true)
+            {
+                ledPort.Write(true);
+                Thread.Sleep(1000);
+
+                ledPort.Write(false);
+                Thread.Sleep(1000);
+            }
+
+
+        }
+
+    }
+}
